901052520c8122534b35b610d8afc626266f0452
satoko123 <moonysweets@gmail.com>
2021-02-05 17:09:35 +0900

    バリデーションの記載を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true do
-    validates :nickname, presence: true
-    validates :birthday, presence: true
+    validates :nickname
+    validates :birthday
   end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'

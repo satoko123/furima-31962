6fb614848c50eeef698c10ac608750e7c141d38b
satoko123 <moonysweets@gmail.com>
2021-03-12 23:26:20 +0900

    テストコードの作成

---
+++ spec/factories/order_forms.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_form do
+    postal_code { '000-0000' }
+    region_id { '2' }
+    city { '横山市緑区' }
+    street_address { '青山1-1-1' }
+    building { 'ビル1' }
+    phone_number { '09012345678' }
+  end
+end

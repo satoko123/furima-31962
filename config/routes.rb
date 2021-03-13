fd5f408fb0e8ca058a7a5076492c76882c26e45c
satoko123 <moonysweets@gmail.com>
2021-03-13 16:00:24 +0900

    重複したルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'orders/index'
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :items do
     resources :orders, only: [:index, :create]
   end

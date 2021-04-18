fb25437f9e5e81b4c7c8270a3cdd3b83895cacd5
satoko123 <moonysweets@gmail.com>
2021-04-18 18:10:23 +0900

    アソシエーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,6 +16,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many_attached :images
   has_one :order
+  has_many :tags, through: :item_tags
+  has_many :item_tags
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category

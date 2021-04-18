7f66aa26c47b52565cf3a125bb0cee3920519154
satoko123 <moonysweets@gmail.com>
2021-04-18 19:48:29 +0900

    Formオブジェクトにsaveメソッド定義

--- app/models/item_tag_form.rb
+++ app/models/item_tag_form.rb
@@ -26,4 +26,12 @@ class TweetTagForm
   validates :price, presence: true, inclusion: { in: 300..9_999_999, message: 'Out of setting range' },
                     format: { with: /\A\d+\z/, message: 'Half-width number' }
 
+  def save
+    item = Item.create(item_name: item_name, item_text: item_text, category_id: category_id,
+                       item_state_id: item_state_id, paying_for_shipping_id: paying_for_shipping_id,
+                       shipping_area_id: shipping_area_id,shipping_day_id: shipping_day_id,price: price,
+                       user_id: user_id,tag_name: tag_name)
+    tag = Tag.create(tag_name: tag_name)
+    ItemTag.create(item_id: item.id, tag_id: tag.id)
+  end
 end
